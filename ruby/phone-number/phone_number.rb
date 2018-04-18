04f519b3e7161d40aa8f141a10e091c0f7968280
J E <jemtrash@gmail.com>
2018-04-18 21:36:20 +1000

    Update to local variables.

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -6,26 +6,25 @@ class PhoneNumber
   def self.clean(input)
     # Remove all characters except integers
     # Catches both letters and special characters
-    @temp = input
-    @result = ''
-    @temp.each_char do |char|
-      @result += char if '0123456789'.include?(char)
-    end
+    result = ''
+    input.each_char { |char| result += char if '0123456789'.include?(char) }
 
     # 11 digit numbers must have a '1' international code or they are invalid
-    if @result.length == 11
-      @result[0] == '1' ? @result = @result[1..10] : return
+    if result.length == 11
+      result[0] == '1' ? result = result[1..10] : return
     end
 
     # Catch any inputs that are too long or short
-    return nil unless @result.length == 10
+    return nil unless result.length == 10
 
     # Ensure Area code and exchange code are valid
-    '23456789'.include?(@result[0]) && '23456789'.include?(@result[3]) ? @result : return
+    '23456789'.include?(result[0]) && '23456789'.include?(result[3]) ? result : return
   end
 
 end
 
+# Required for Exercism.io
+# Would usually be in its own string
 module BookKeeping
   VERSION = 2 # Where the version number matches the one in the test.
 end
